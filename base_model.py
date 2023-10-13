ac510adb4b2166026ab7f685bed99f88f8f0bfe7
MarwaAshraf1812 <marashraf090@gmail.com>
2023-10-14 00:51:06 +0300

    h

--- base_model.py
+++ base_model.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
-from uuid import uuid4, isoformat
+"""
+defines all common attributes/methods for other classes
+"""
+
+from uuid import uuid4
 from datetime import datetime
 from __init__ import storage
 
+
 class BaseModel():
+    """
+    Represents the BaseModel of the AirBnB project.
+    """
 
     def __init__(self, *args, **kwargs):
-        
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         standard = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -23,24 +39,25 @@ class BaseModel():
             storage.new(self)
 
     def __str__(self):
+        """
+        Return the print/str representation of the BaseModel instance.
+        """
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-    
 
     def save(self):
+        """
+        Update updated_at with the current datetime.
+        """
         self.updated_at = datetime.now()
         storage.save()
-        
 
     def to_dict(self):
+        """
+        returns a dictionary containing all 
+        keys/values of __dict__ of the instance
+        """
         dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
         dict["created_at"] = str(dict["created_at"].isoformat())
         dict["updated_at"] = str(dict["updated_at"].isoformat())
-
-        # for key, value in dict.items():
-        #     if isinstance(value, datetime):
-        #         dict[key] = value.isoformat()
-
         return dict
-    
-    
--- engine/file_storage.py
+++ engine/file_storage.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
+"""
+Defines the FileStorage class.
+"""
 import json
-from datetime import datetime
+
+
 class FileStorage:
+    """
+    Represent an abstracted storage engine.
+
+    Attr:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
+        """
+        returns the dictionary __objects
+        """
         return FileStorage.__objects
     
     def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
         FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-        
+        """
+        Serializes __objects to the JSON file 
+        """
         data_to_save = {key: value.__dict__ for key, value in FileStorage.__objects.items()}
 
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(data_to_save, file)
 
     def reload(self):
+        """
+        Deserializes the JSON file to __objects, if it exists.
+        """
         if FileStorage.__file_path:
             try:
                 with open(FileStorage.__file_path, 'r') as file:
                     FileStorage.__objects = json.load(file)
-                    for v in self.__objects.items():
+                    for v in FileStorage.__objects.items():
                         className = v["__class__"]
                         #using the eval function to dynamically create an instance of a class based on the class name
-                        self.new(eval(className)(**v))
+                        FileStorage.new(eval(className)(**v))
             except FileNotFoundError:
                 pass
